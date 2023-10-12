63ea98e4b4445d1d227c7e4858ec5a711ef1d27a
Raquel <rachel050479@gmail.com>
2023-10-12 18:24:42 +0200

    added last exercise

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,6 @@ function App() {
 
   const handleClick = () => {
     setNumberOfErrors(numberOfErrors + 1);
-    return numberOfErrors;
   };
 
   const handleInputLastLetter = (event) => {
@@ -17,97 +16,106 @@ function App() {
     let re = /^[A-Za-zñÑ-á-úÁ-Ú]+$/;
     if (re.test(letter) || letter === "") {
       setLastLetter(letter);
+      if (letter && word.includes(letter)) {
+        setUserLetters([...userLetters, letter]);
+      }
     }
-    if (re.test(letter) && letter !== "") {
-      setLastLetter([letter, ...userLetters]);
-    }
+  };
 
-    const renderSolutionLetters = () => {
-      const wordLetters = word.split("");
-      return wordLetters
-        .filter((letter) =>
-          letter.toLowerCase().includes(lastLetter.toLowerCase())
-        )
-        .map((letter, index) => {
-          return <li key={index} className="letter"></li>;
-        });
-    };
+  const renderSolutionLetters = () => {
+    const wordLetters = word.split("");
+    return wordLetters.map((letter, index) => {
+      const isletter = userLetters.includes(letter.toLocaleLowerCase());
+      return (
+        <li key={index} className="letter">
+          {isletter ? letter : ""}
+        </li>
+      );
+    });
+  };
 
-    const handleSubmit = (event) => {
-      event.preventDefault();
-    };
+{/*
+  const renderErrorLetters = () => {
+    const errorLetter = userLetters.filter(
+      (letter) => !word.toLowerCase().includes(letter.toLowerCase())
+    );
+    return errorLetter.map((letter, index) => {
+      return (
+        <li key={index} className="letter">{letter}</li>
+      );
+      
+    });
+  };
+*/}
 
-    return (
-      <>
-        <div className="page">
-          <header>
-            <h1 className="header__title">Juego del ahorcado</h1>
-          </header>
-          <main className="main">
-            <section>
-              <div className="solution">
-                <h2 className="title">Solución:</h2>
+  const handleSubmit = (event) => {
+    event.preventDefault();
+  };
 
-                <ul className="letters">
-                  {renderSolutionLetters()}
-                  {/*}  <li className='letter'>p</li>
+  return (
+    <>
+      <div className="page">
+        <header>
+          <h1 className="header__title">Juego del ahorcado</h1>
+        </header>
+        <main className="main">
+          <section>
+            <div className="solution">
+              <h2 className="title">Solución:</h2>
+
+              <ul className="letters">
+                {renderSolutionLetters()}
+                {/*}  <li className='letter'>p</li>
                 <li className='letter'>e</li>
                 <li className='letter'>p</li>
                 <li className='letter'></li>
                 <li className='letter'>n</li>
                 <li className='letter'></li>*/}
-                </ul>
-              </div>
-              <div className="error">
-                <h2 className="title">Letras falladas:</h2>
-                <ul className="letters">
-                  <li className="letter">f</li>
-                  <li className="letter">q</li>
-                  <li className="letter">h</li>
-                  <li className="letter">p</li>
-                  <li className="letter">x</li>
-                </ul>
-              </div>
-              <form className="form" onSubmit={handleSubmit}>
-                <label className="title" htmlFor="last-letter">
-                  Escribe una letra:
-                </label>
-                <input
-                  autoComplete="off"
-                  className="form__input"
-                  maxLength="1"
-                  type="text"
-                  name="last-letter"
-                  id="last-letter"
-                  onChange={handleInputLastLetter}
-                  value={lastLetter}
-                />
-              </form>
-            </section>
+              </ul>
+            </div>
+            <div className="error">
+              <h2 className="title">Letras falladas:</h2>
+              <ul className="letters"></ul>
+            </div>
+            <form className="form" onSubmit={handleSubmit}>
+              <label className="title" htmlFor="last-letter">
+                Escribe una letra:
+              </label>
+              <input
+                autoComplete="off"
+                className="form__input"
+                maxLength="1"
+                type="text"
+                name="last-letter"
+                id="last-letter"
+                onChange={handleInputLastLetter}
+                value={lastLetter}
+              />
+            </form>
+          </section>
 
-            <section className={`dummy error-${numberOfErrors}`}>
-              <span className="error-13 eye"></span>
-              <span className="error-12 eye"></span>
-              <span className="error-11 line"></span>
-              <span className="error-10 line"></span>
-              <span className="error-9 line"></span>
-              <span className="error-8 line"></span>
-              <span className="error-7 line"></span>
-              <span className="error-6 head"></span>
-              <span className="error-5 line"></span>
-              <span className="error-4 line"></span>
-              <span className="error-3 line"></span>
-              <span className="error-2 line"></span>
-              <span className="error-1 line"></span>
-            </section>
-          </main>
-          <button onClick={handleClick} className="btnInc">
-            INCREMENTAR
-          </button>
-        </div>
-      </>
-    );
-  };
+          <section className={`dummy error-${numberOfErrors}`}>
+            <span className="error-13 eye"></span>
+            <span className="error-12 eye"></span>
+            <span className="error-11 line"></span>
+            <span className="error-10 line"></span>
+            <span className="error-9 line"></span>
+            <span className="error-8 line"></span>
+            <span className="error-7 line"></span>
+            <span className="error-6 head"></span>
+            <span className="error-5 line"></span>
+            <span className="error-4 line"></span>
+            <span className="error-3 line"></span>
+            <span className="error-2 line"></span>
+            <span className="error-1 line"></span>
+          </section>
+        </main>
+        <button onClick={handleClick} className="btnInc">
+          INCREMENTAR
+        </button>
+      </div>
+    </>
+  );
 }
 
 export default App;
