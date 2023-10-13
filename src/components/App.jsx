01e1153b2cfe2c3c2b6c43afa008f2b7e535fa2d
Raquel <rachel050479@gmail.com>
2023-10-13 11:24:25 +0200

    added exercise fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,20 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "../styles/main.scss";
 
 function App() {
   const [numberOfErrors, setNumberOfErrors] = useState(0);
   const [lastLetter, setLastLetter] = useState("");
-  const [word, setWord] = useState("pepina");
+  const [word, setWord] = useState("");
   const [userLetters, setUserLetters] = useState([]);
+
+  useEffect(() => {
+    fetch("https://dev.adalab.es/api/random/word")
+      .then((response) => response.json())
+      .then((data) => {
+        setWord(data.word);
+        console.log(data.word);
+      });
+  }, []);
 
   const handleClick = () => {
     setNumberOfErrors(numberOfErrors + 1);
@@ -25,7 +34,7 @@ function App() {
   const renderSolutionLetters = () => {
     const wordLetters = word.split("");
     return wordLetters.map((letter, index) => {
-      const isletter = userLetters.includes(letter.toLocaleLowerCase());
+      const isletter = userLetters.toLowerCase().includes(letter.toLowerCase());
       return (
         <li key={index} className="letter">
           {isletter ? letter : ""}
@@ -34,7 +43,8 @@ function App() {
     });
   };
 
-{/*
+  {
+    /*
   const renderErrorLetters = () => {
     const errorLetter = userLetters.filter(
       (letter) => !word.toLowerCase().includes(letter.toLowerCase())
@@ -46,7 +56,8 @@ function App() {
       
     });
   };
-*/}
+*/
+  }
 
   const handleSubmit = (event) => {
     event.preventDefault();
